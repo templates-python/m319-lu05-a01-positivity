a8d0ea69b61cd6a00fc17fdb455f9cda49f007c5
Marcel Suter <marcel.suter@bzz.ch>
2023-07-19 08:12:04 +0200

    initial commit

--- positivity.py
+++ positivity.py
@@ -1,9 +1,6 @@
 def main():
-    number = int(input('Give a number:\n'))
-    if number > 0:
-        print('The number is positive.')
-    else:
-        print('The number is not positive.')
+    # Write your program here and remove the line "pass"
+    pass
 
 
 if __name__ == '__main__':
