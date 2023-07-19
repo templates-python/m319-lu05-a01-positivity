3a64249e1a95a2b719159942c6c19b0dbf6ac796
Marcel Suter <marcel.suter@bzz.ch>
2023-07-19 08:04:57 +0200

    initial commit

---
+++ positivity.py
@@ -0,0 +1,10 @@
+def main():
+    number = int(input('Give a number:\n'))
+    if number > 0:
+        print('The number is positive.')
+    else:
+        print('The number is not positive.')
+
+
+if __name__ == '__main__':
+    main()
